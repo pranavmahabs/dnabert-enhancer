ea510116ea93e2e34f19f047a46323eba149ba80
pranavmahabs <pranav_mahableshwarkar@brown.edu>
2023-06-15 15:59:54 -0400

    Started Building Model

---
+++ model/model.py
@@ -0,0 +1,19 @@
+from layers import EncoderLayer
+from embed import Embedder, PositionalEncoder
+
+class GenoScanner(nn.Module):
+    def __init__(self, vocab_size, d_model, N, heads, dropout):
+        super().__init__()
+        self.N = n
+        self.embed = Embedder(vocab_size, d_model)
+        self.pe = PositionalEncoder(d_model, dropout=dropout)
+        self.layers = get_clones(EncoderLayer(d_model, heads, droupout), N)
+        self.norm = Norm(d_model)
+
+        ## Classifier
+        self.ff = self.Linear(d_model, d_ff)
+        self.drop = nn.dropout(dropout)
+        self.ff2 = self.Linear(d_model, 3)
+    
+    def forward(self, x):
+
