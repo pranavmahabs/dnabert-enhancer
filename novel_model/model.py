00e5ea09687b74243f8e751185621abef3bc9b4c
pranavmahabs <pranav_mahableshwarkar@brown.edu>
2023-06-20 16:35:10 -0400

    Finished Model Imp. Prepped Train/Test Code

--- novel_model/model.py
+++ novel_model/model.py
@@ -1,22 +1,20 @@
 import torch
-import torch.nn
-from layers import EncoderLayer
+import torch.nn as nn
 from embed import Embedder, PositionalEncoder
+from transformer import TransformerBlock
 
 class GenoScanner(nn.Module):
     def __init__(self, vocab_size, d_model, N, heads, dropout):
         super().__init__()
-        self.N = n
-        self.embed = Embedder(vocab_size, d_model)
-        self.pe = PositionalEncoder(d_model, dropout=dropout)
-        self.layers = get_clones(EncoderLayer(d_model, heads, droupout), N)
-        self.norm = Norm(d_model)
+        self.transformer = TransformerBlock(vocab_size, d_model, heads, d_model, N, dropout)
 
         ## Classifier
         self.ff = self.Linear(d_model, d_ff)
         self.drop = nn.dropout(dropout)
         self.ff2 = self.Linear(d_model, 3)
     
-    def forward(self, x):
-        pass
-
+    def forward(self, x, x_mask):
+        src = self.transformer(x, x_mask)
+        ff1 = self.drop(self.ff(src))
+        logits = nn.Softmax(self.ff2(ff1))
+        return logits
---
+++ novel_model/transformer.py
@@ -0,0 +1,27 @@
+import math
+import torch
+from torch import nn, Tensor
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from embed import PositionalEncoding
+
+class TransformerBlock(nn.Module):
+    def __init__(self, ntoken, d_model, nhead, d_hid, nlayers, dropout):
+        super().__init__()
+        self.model_type = "TransformerBlock"
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, d_model)
+        self.d_model = d_model
+
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, x, x_mask):
+        x = self.embedding(x) * math.sqrt(self.d_model)
+        x = self.pos_encoder(x)
+        output = self.transformer_encoder(x, x_mask)
+        return output
+
